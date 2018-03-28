153220a99f2a10cb09c052494e38b5141d37f55e
David Riser <davidriser@Davids-iMac.fios-router.home>
2018-03-28 10:42:44 -0400

    Adding randomized parameters, and making replica-fitter parallel.

--- kaon-bsa/replica-fitter.py
+++ kaon-bsa/replica-fitter.py
@@ -2,8 +2,11 @@
 
 import argparse
 import numpy as np 
+import os 
 import pandas as pd
 import tqdm 
+
+from multiprocessing import Process, Queue
 
 # single fit minimizer 
 from scipy.optimize import minimize 
@@ -15,16 +18,64 @@ def create_replica(y, y_err):
     y_rep = [np.random.normal(yp,np.fabs(yp_err)) for yp,yp_err in zip(y,y_err)]
     return np.array(y_rep)
 
-def perform_bootstrap(loss_function, physics_model, bounds,
-                      phi, data, error, n_replicas=20):
+def bootstrap_worker(q, loss_function, physics_model, bounds,
+                     phi, data, error, n_replicas=20):
+
+    np.random.seed(os.getpid())
+
     results = []
     for irep in tqdm.tqdm(range(n_replicas)):
         rep = create_replica(data, error) 
         pars,errs = perform_single(loss_function, physics_model, bounds, phi, rep, error)
         results.append(pars)
 
-    
-    results = np.array(results, dtype=np.float32)
+    q.put(results)
+
+def bootstrap(loss_function, physics_model, bounds,
+              phi, data, error, n_replicas=20):
+
+    results = []
+    for irep in tqdm.tqdm(range(n_replicas)):
+        rep = create_replica(data, error) 
+        pars,errs = perform_single(loss_function, physics_model, bounds, phi, rep, error)
+        results.append(pars)
+
+    return results
+
+def perform_bootstrap(loss_function, physics_model, bounds,
+                      phi, data, error, n_replicas=20, n_cores=4):
+
+    if n_cores is 1:
+        results = bootstrap(loss_function, physics_model, bounds, 
+                            phi, data, error, n_replicas)
+        results = np.array(results, dtype=np.float32)
+
+    elif n_cores > 1:
+        q = Queue() 
+        workers = []
+
+        # spawn processes 
+        reps_per_core = int(n_replicas/n_cores)
+        for job in range(n_cores):
+            workers.append(Process(target=bootstrap_worker, args=(q, loss_function, physics_model, bounds, phi, data, error, reps_per_core)))
+            workers[job].start()
+        
+        # get results     
+        result_pool = []
+        for worker in workers:
+            rv = q.get() 
+            result_pool.append(rv)
+            
+        # end 
+        for worker in workers:
+            worker.join() 
+
+        # aggregate results so they look normal 
+        results = [item for sublist in result_pool for item in sublist]
+        results = np.array(results, dtype=np.float32)
+
+    else:
+        print('What are you trying to do asking for %d cores?' % n_cores)
 
     pars = []
     errs = []
@@ -56,7 +107,7 @@ def perform_single(loss_function, model, bounds,
     return result.x, err[0]    
 
 
-def fit(input_file, output_file, bounds, n_reps):
+def fit(input_file, output_file, bounds, n_reps, n_proc):
 
     # load dataset 
     dataset = pd.read_csv(input_file)
@@ -85,7 +136,7 @@ def fit(input_file, output_file, bounds, n_reps):
 
             # perform single fitting 
             pars, errs = perform_bootstrap(loss_function, physics_model, bounds, 
-                                           data.phi, data.value, data.stat, n_reps)
+                                           data.phi, data.value, data.stat, n_reps, n_proc)
 
             output_data['axis'].append(axis)
             output_data['axis_bin'].append(axis_bin)
@@ -110,6 +161,7 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output_file', required=True)
     parser.add_argument('-b', '--bounded', default='no')
     parser.add_argument('-n', '--n_replicas', default=100, type=int)
+    parser.add_argument('-m', '--n_proc', default=4, type=int)
     args = parser.parse_args()
 
 
@@ -123,4 +175,4 @@ if __name__ == "__main__":
     else:
         bounds = [[-1,1],[-1,1],[-1,1]]
 
-    fit(args.input_file, args.output_file, bounds, args.n_replicas)
+    fit(args.input_file, args.output_file, bounds, args.n_replicas, args.n_proc)
