d8a8521deb30a831f2429807c1bba92ca6c1ca10
David Riser <dmriser@jlab.org>
2018-11-16 13:39:40 -0500

    adding files for fiting

--- kaon-bsa/src/data/utils.py
+++ kaon-bsa/src/data/utils.py
@@ -102,9 +102,9 @@ def build_filter(data, conf=None):
     nominal_conf['dist_ecw'] = [-1.0, 1.0]
     nominal_conf['dist_ec_edep'] = [-1.0, 1.0]
     nominal_conf['dist_vz'] = [-1.0, 1.0]
-    nominal_conf['missing_mass'] = [1.575, 5.0]
+    nominal_conf['missing_mass'] = [1.62, 5.0]
     nominal_conf['p_mes'] = [0.35, 5.0]
-    nominal_conf['dvz'] = [-2.5, 2.5]
+    nominal_conf['dvz'] = [-4.0, 4.0]
 
     # start adding the special options
     if conf:
