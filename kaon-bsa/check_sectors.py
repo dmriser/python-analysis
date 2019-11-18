cd583e53f497044c8f99f1ae2899a07ae43f4976
David Riser <davidriser@Davids-iMac.local>
2019-11-18 07:58:51 -0500

    Added a feature for producing more informative phi compare plots for sector difference

--- kaon-bsa/check_sectors.py
+++ kaon-bsa/check_sectors.py
@@ -9,42 +9,48 @@ def chi2(data1, err1, data2, err2):
 
 if __name__ == '__main__':
 
-    data = {}
-    data[1] = pd.read_csv('database/phi/sector_1.csv').query('axis == "x"')
-    data[2] = pd.read_csv('database/phi/sector_2.csv').query('axis == "x"')
-    data[3] = pd.read_csv('database/phi/sector_3.csv').query('axis == "x"')
-    data[4] = pd.read_csv('database/phi/sector_4.csv').query('axis == "x"')
-    data[5] = pd.read_csv('database/phi/sector_5.csv').query('axis == "x"')
-    data[6] = pd.read_csv('database/phi/sector_6.csv').query('axis == "x"')
+    for axis in ['x', 'q2', 'z', 'pt']:
+        data = {}
+        data[1] = pd.read_csv('database/phi/sector_1.csv').query('axis == "{}"'.format(axis))
+        data[2] = pd.read_csv('database/phi/sector_2.csv').query('axis == "{}"'.format(axis))
+        data[3] = pd.read_csv('database/phi/sector_3.csv').query('axis == "{}"'.format(axis))
+        data[4] = pd.read_csv('database/phi/sector_4.csv').query('axis == "{}"'.format(axis))
+        data[5] = pd.read_csv('database/phi/sector_5.csv').query('axis == "{}"'.format(axis))
+        data[6] = pd.read_csv('database/phi/sector_6.csv').query('axis == "{}"'.format(axis))
 
 
-    pad_idx = 1
-    for i in range(1,7):
-        for j in range(1,7):
-            #plt.subplot(6,6,pad_idx)
-            #plt.scatter(data[i]['value'], data[j]['value'], edgecolor='k', alpha=0.7)
-            #pad_idx += 1
+        pad_idx = 1
+        for i in range(1,7):
+            for j in range(1,7): 
+                metric = chi2(data[i]['value'], data[i]['stat'],
+                              data[j]['value'], data[j]['stat'])
+                print('Sectors ({}/{}) w/ chi2 = {}'.format(i,j,metric))
 
-            #metric = chi2(data[i]['value'], data[i]['stat'],
-            #              data[j]['value'], data[j]['stat'])
-            #print('Sectors ({}/{}) w/ chi2 = {}'.format(i,j,metric))
-
-            if i != j:
-                residual = data[i]['value'] - data[j]['value']
-
-                plt.figure(figsize=(8,6))
-                plt.subplot(2,1,1)
-                plt.errorbar(data[i]['global_index'], data[i]['value'], data[i]['stat'], linestyle='',
-                            marker='o', color='orange', alpha=0.8)
-                plt.errorbar(data[j]['global_index'], data[j]['value'], data[j]['stat'], linestyle='',
-                            marker='o', color='purple', alpha=0.8)
-                plt.grid(alpha=0.1)
-
-                plt.subplot(2,1,2)
-                plt.hist(residual, bins=np.linspace(-0.2, 0.2, 20), edgecolor='k', color='orange', alpha=0.6)
+                if i != j:
+                    xbins = data[i]['axis_bin'].unique()
                 
-                plt.savefig('image/sectors/compare_{}_{}.pdf'.format(i,j))
-                plt.close()
-                
-        pad_idx += 1
-            
+                    plt.figure(figsize=(16,6))
+
+                    for ii, xbin in enumerate(xbins):
+                        top = data[i].query('axis_bin == {}'.format(xbin))
+                        bot = data[j].query('axis_bin == {}'.format(xbin))
+                        residual = top['value'] - bot['value']
+
+                        plt.subplot(2, len(xbins), ii+1)
+                        plt.errorbar(top['phi'], top['value'], top['stat'], linestyle='',
+                                     marker='o', color='red', alpha=0.8)
+                        plt.errorbar(bot['phi'], bot['value'], bot['stat'], linestyle='',
+                                     marker='o', color='blue', alpha=0.8)
+                        plt.axhline(0, linestyle='--', linewidth=1, color='k')
+                        plt.grid(alpha=0.1)
+                        plt.title('Sectors ({},{}), Bin {}, Axis {}'.format(
+                            i, j, xbin, axis
+                        ))
+                    
+                        plt.subplot(2, len(xbins), ii + 1 + len(xbins))
+                        plt.hist(residual, bins=np.linspace(-0.2, 0.2, 15), edgecolor='k', color='red', alpha=0.6)
+                    
+                    plt.tight_layout()
+                    plt.savefig('image/sectors/compare_{}_{}_{}.pdf'.format(axis,i,j))
+                    plt.close()
+        
